d5a49acdfca7fc5bb000c7ebad6f98723acf24be
adm_ervv <v.ershova@magdv.com>
2025-10-20 16:36:58 +0300

    Update project files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	alias(libs.plugins.android.application)
-	alias(libs.plugins.kotlin.android)
+	id("com.android.application")
+	id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -59,7 +59,7 @@ android {
 
 	buildFeatures { compose = true }
 
-	composeOptions { kotlinCompilerExtensionVersion = libs.versions.composeCompilerExtension.get() }
+	composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
 
 	packaging {
 		resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -67,15 +67,15 @@ android {
 }
 
 dependencies {
-	implementation(platform(libs.androidx.compose.bom))
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.lifecycle.runtime.ktx)
-	implementation(libs.androidx.activity.compose)
-	implementation(libs.androidx.compose.ui)
-	implementation(libs.androidx.compose.ui.graphics)
-	implementation(libs.androidx.compose.ui.tooling.preview)
-	implementation(libs.androidx.compose.material3)
-	implementation(libs.coil.compose)
-	implementation(libs.accompanist.systemuicontroller)
-	debugImplementation(libs.androidx.compose.ui.tooling)
+	implementation(platform("androidx.compose:compose-bom:2024.09.03"))
+	implementation("androidx.core:core-ktx:1.13.1")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+	implementation("androidx.activity:activity-compose:1.9.3")
+	implementation("androidx.compose.ui:ui")
+	implementation("androidx.compose.ui:ui-graphics")
+	implementation("androidx.compose.ui:ui-tooling-preview")
+	implementation("androidx.compose.material3:material3:1.3.0")
+	implementation("io.coil-kt:coil-compose:2.6.0")
+	implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
+	debugImplementation("androidx.compose.ui:ui-tooling")
 }
